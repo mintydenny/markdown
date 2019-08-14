f5e6c855123624c89a65aef0a28b69e688b231bb
mintydenny <denny0489@gmail.com>
2019-08-13 22:23:57 -0700

    Updates

---
+++ precache-manifest.6894019cda060a5b85f22f4e593e5226.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4078b8f22705ba77ff34d24f8486968",
+    "url": "/Markdown/index.html"
+  },
+  {
+    "revision": "907c2474ddd1fc29d836",
+    "url": "/Markdown/static/css/main.90866b1b.chunk.css"
+  },
+  {
+    "revision": "5c30d612d70c90000a7b",
+    "url": "/Markdown/static/js/2.0ecd76cb.chunk.js"
+  },
+  {
+    "revision": "907c2474ddd1fc29d836",
+    "url": "/Markdown/static/js/main.59736991.chunk.js"
+  },
+  {
+    "revision": "865f83ade5d28b6354ab",
+    "url": "/Markdown/static/js/runtime~main.3f3e0214.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Markdown/static/media/logo.ee7cd8ed.svg"
+  }
+]);
